7c27f9d57c722e7bdb30c5cd32ead247dd21ad8e
josue <L21010938@outlook.com>
2025-10-22 17:58:57 -0600

    subir pagina

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Josue Cruz",
+  title: "ING en sistenas computacionales",
+  email: "L21010938@gmail.com",
+  gitHub: "josue123169",
+  instagram: "josue_cru",
+  linkedIn: "josue-crsuz",
   medium: "",
   twitter: "microsoft",
-  youTube: "Code",
+  youTube: "Josuehola",
 };
 
 const primaryColor = "#4E567E";
